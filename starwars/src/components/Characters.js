c70db801b466190a1e3a930699ec0ce371f9eb8e
unknown <kenneth.austin93@gmail.com>
2019-07-19 14:19:50 -0400

    needs to add Characters component and map

---
+++ starwars/src/components/Characters.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export default function Characters({name}) {
+    return (
+        <div className = "Characters">
+            <h3>{name}</h3>
+        </div> 
+    )
+}
